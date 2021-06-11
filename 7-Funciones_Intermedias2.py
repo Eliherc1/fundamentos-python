d5a5ff053962bf9eb64faa1f00186ddd484ea0ba
Eliherc1 <elidezx@gmail.com>
2021-06-10 22:37:31 -0400

    actualizacion

--- 7-Funciones_Intermedias2.py
+++ 7-Funciones_Intermedias2.py
@@ -1,6 +1,7 @@
 #Actualiza los valores en diccionarios y listas
 
 x = [ [5,2,3], [10,8,9] ] 
+
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'}
@@ -41,12 +42,17 @@ print(newList)
 
 #En el directorio sports_directory, cambia 'Messi' a 'Andres'
 
+#sports_directory['soccer'][0]= 'andres'  #cambiar directo el nombre
+
 def deportes(sports_directory):
     for valor in sports_directory:
-        print(valor)
+        for sp in range(len(sports_directory[valor])):
+            if sports_directory[valor][sp]=='Messi':
+                sports_directory[valor][sp]= 'Andres'
+    print(sports_directory)
        
 
-
+##diccionario
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
@@ -54,21 +60,34 @@ sports_directory = {
 newArreglo = deportes(sports_directory)
 print(newArreglo)
 
+
+
 #Camba el valor 20 en z a 303
 
-def cambio(xlista):
-    for i in xlista:
-        print(i)
-        for ii in i.values():
-            print(ii)
-            if xlista[i][ii]==20:
-                xlista[i][ii]=303
-            #if xlista[i][ii]==10:
-                #xlista[i][ii]=15
-    return xlista
+def cambiar(lista):
+    for valor in lista:
+        #print(valor['y'])
+        valor['y']=303
+    return lista
+          
 
 z = [ {'x': 10, 'y': 20} ]
-cambio(z)
+print(cambiar(z))
+
+
+def listaEst(students):
+    for est in students:
+        #print(est['last_name'])
+        if est['last_name'] == 'Jordan':
+            est['last_name']='Bryant'
+    return  students
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+newList = listaEst(students)
+print(newList)
 
 #2. Itera a través de una lista de diccionarios
 # Crea una función iterateDictionary(some_list)que, dada una lista de diccionarios, 
@@ -123,18 +142,16 @@ iterateDictionary2(students)
 # Crea una función printInfo(some_dict)que, dado un diccionario cuyos valores son todas listas, imprima el nombre de cada clave 
 # junto con el tamaño de su lista, y luego imprima los valores asociados dentro de la lista de cada clave. Por ejemplo:
 
+
 def printInfo(lista):
-    for valor in lista.values():
-        print(len(valor))
-        for ciudad in valor:
-            print(ciudad)
-
-
-
+    for valor in lista:
+        print(len(valor), valor)
+        #print(valor)
+        for ciudad in range(len(lista[valor])):
+            print(lista[valor][ciudad])       
 
 dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
 printInfo(dojo)
-
